b47c7b9288bcdc6a338a526a47915420e3501ab4
arne ziegler <arne.ziegler@gmx.net>
2023-11-19 18:07:51 +0100

    gridTravel Update with glib

--- dyprg/gridTravel/gridtravel2.c
+++ dyprg/gridTravel/gridtravel2.c
@@ -8,6 +8,7 @@
      gridTraveler (1,0) -> 0 wenn eine Dimension 0 ist, dann ist die Lösung 0
      gridTraveler (3,3) -> Wenn man nun z.B. DRDR geht, dann ist jeweils eine Teillösung in
                            gridTraveler(3,3) -> gridTraveler(2,3) -> gridTraveler(2,2) -> gridTraveler(2,1) -> gridTraveler(1,1)
+     gridTraveler (18,18)->2333606220
 
     Komplexität
       O(time)      2^(n+m)
@@ -15,54 +16,49 @@
    */
 #include <stdio.h>
 #include <string.h>
+#include <glib.h>
 
-#define COUNT 100
+GHashTable *hash;
 
-struct s_matrix
-{
-    char *key[6];
-    int value;
-};
-
-struct s_matrix matrix[COUNT];
-
-long gT(int columns, int rows, int iter);
+long gT(int columns, int rows);
 
 int main()
 {
-    int n, m, iter = 0;
-    unsigned long paths;
+    int n, m;
+    glong paths;
+    // initialize hash table
+    hash = g_hash_table_new(g_str_hash, g_int_equal);
 
     printf("\nTraveler Problem:\n");
     printf("\nAnzahl Spalten: ");
     scanf("%d", &n);
     printf("\nAnzahl Zeilen: ");
     scanf("%d", &m);
-    paths = gT(n, m, iter);
+    paths = gT(n, m);
     printf("\nAnzahl Pfade: %ld\n", paths);
 
     return 0;
 }
 
-long gT(int col, int row, int iter)
+glong gT(int col, int row)
 {
-    char tmp[6]; /* key = "m,n" */
-    sprintf(tmp, "%d,%d", row, col);
-    printf("\n String = %s", tmp);
+    glong *value = g_new(glong, 1);
+    char key[6]; /* key = "m,n" */
+    sprintf(key, "%d,%d", row, col);
+    printf("\n Key = %s", key);
 
-    /*if (key in memo)
-        return memo(key);
-*/
+    // check if key in hash table
+    if (g_hash_table_lookup(hash, key) != NULL)
+        return *(glong *)g_hash_table_lookup(hash, key);
     if (col == 1 || row == 1)
         return 1;
     if (col == 0 || row == 0)
         return 0;
 
-    *matrix[iter].key = tmp;
-    matrix[iter].value = 5;
-
+    *value = gT(col - 1, row) + gT(col, row - 1);
+    g_hash_table_insert(hash, key, value);
     // memo[key] = (gT(col - 1, row, hash) + gT(col, row - 1, hash));
-    // return memo[key];
+    return *value;
 
     return 0;
 }
