1fc7a246690988d3f39acbca0cc453405b457de6
arne ziegler <arne.ziegler@gmx.net>
2023-12-08 22:44:54 +0100

    update howsum2

--- dyprg/howsum/howsum2.c
+++ dyprg/howsum/howsum2.c
@@ -3,8 +3,8 @@ write a function "howSum(targetSum, numbers)" that takes in a
 targetSum and an array of numbers as arguments.
 
 The function should return an array containing any combinations of elements
-that add up to exactly the targetSum. If there is no combination that adds up to the
-targetSum, then return null.
+that add up to exactly the targetSum. If there is no combination that adds up to
+the targetSum, then return null.
 
 If there are multiple combinations possible, you may return any single one.
 
@@ -21,130 +21,113 @@ Complexity:
   space = O(m)
 
 
-hash
- key=string - value = 7
- 2,2,3 = 7
+result = glist 3->2->2
+
+
 
 */
 
-#include <stdio.h>
-#include <stdbool.h>
 #include <glib.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #define MAXSIZE 10
 
-int array[MAXSIZE];
-GSList *value;
+void print_iterator(gpointer item) { printf("%d\n", GPOINTER_TO_INT(item)); }
 
-void printArray(int targetSum, int *array, int maxvalues)
-{
-    int i = 0;
-    for (i = 0; i < maxvalues; i++)
-        printf("%d,", array[i]);
-    return;
+int howSum(int targetSum, gpointer array, gpointer result) {
+  printf("\ntargetSum=%d", targetSum);
+  if (targetSum == 0) {
+    g_slist_foreach(result, (GFunc)print_iterator, NULL);
+    return result;
+  }
+
+  if (targetSum < 0) return NULL;
+
+  // 7 [2,3]   [3,2,2]
+
+  GSList *num = NULL;
+  int remainder = 0;
+
+  for (num = array; num; num = num->next) {
+    int numdata = GPOINTER_TO_INT(num->data);
+    remainder = targetSum - numdata;
+    printf("\nremainder = targetSum - numdata : %d = %d - %d", remainder,
+           targetSum, numdata);
+
+    int remainderResult = howSum(remainder, array, result);
+
+    if (remainderResult != NULL) {
+      printf("Reminder Result !=0");
+      result = g_slist_append(result, GPOINTER_TO_INT(remainderResult));
+      return result;
+    }
+  }
+  return NULL;
 }
 
-void printArray2()
-{
-    GSList *iterator = NULL;
-    for (iterator = value; iterator; iterator = value->next)
-    {
-        printf("\nCurrent item is '%d'\n", *(int *)value->data);
-    }
-    return;
-}
+int main() {
+  GSList *result = NULL;  // list of results
+  GSList *array = NULL;   // list of available numbers
 
-int *howSum(int targetSum, int *array, int maxvalues)
-{
-    gint *tmpval = g_new(gint, 1);
-    printf("\ntargetSum=%d", targetSum);
-    if (targetSum == 0)
-    {
-        printf("\nPrint value\n");
-        printArray2();
-        return g_list_last(value);
-    }
+  // initialize array and print
+  array = g_slist_append(array, (int *)2);
+  array = g_slist_append(array, (int *)3);
+  g_slist_foreach(array, (GFunc)print_iterator, NULL);
+  howSum(7, array, result);
 
-    if (targetSum < 0)
-        return NULL;
-
-    int num = 0;
-    int remainder = 0;
-    for (num = 0; num < maxvalues; num++)
-    {
-        remainder = targetSum - array[num];
-
-        int *remainderResult = howSum(remainder, array, maxvalues);
-        if (remainderResult != NULL)
-        {
-            printf("Reminder Result !=0");
-            *tmpval = remainderResult;
-            g_slist_append(value, tmpval);
-            return remainderResult;
-        }
-    }
-    return NULL;
-}
-
-int main()
-{
-    GSList *value = NULL; // list of values
-
-    int array[MAXSIZE] = {2, 3};
+  /*
     printf("\nhowSum 7 of array [");
-    printArray(7, array, 2);
-    if (howSum(7, array, 2) == 0)
-    {
-        printf("] is [");
-        printArray2();
+
+    if (howSum(7, array, 2) == 0) {
+      printf("] is [");
+      printArray2();
+    } else {
+      printf("] has no solution.");
     }
-    else
-    {
-        printf("] has no solution.");
-    }
+  */
+  // resultcnt = 0;
+  // array[0] = 5;
+  // array[1] = 3;
+  // array[2] = 4;
+  // array[3] = 7;
+  // printf("\nhowSum 7 of array [");
+  // printArray(7, array, 4);
+  // howSum(7, array, 4);
+  // printf("] is [");
+  // printArray(7, result, resultcnt);
+  // printf("]\n\n");
 
-    // resultcnt = 0;
-    // array[0] = 5;
-    // array[1] = 3;
-    // array[2] = 4;
-    // array[3] = 7;
-    // printf("\nhowSum 7 of array [");
-    // printArray(7, array, 4);
-    // howSum(7, array, 4);
-    // printf("] is [");
-    // printArray(7, result, resultcnt);
-    // printf("]\n\n");
+  // resultcnt = 0;
+  // array[0] = 2;
+  // array[1] = 4;
+  // printf("\nhowSum 7 of array [");
+  // printArray(7, array, 2);
+  // howSum(7, array, 2);
+  // printf("] is [");
+  // printArray(7, result, resultcnt);
+  // printf("]\n\n");
 
-    // resultcnt = 0;
-    // array[0] = 2;
-    // array[1] = 4;
-    // printf("\nhowSum 7 of array [");
-    // printArray(7, array, 2);
-    // howSum(7, array, 2);
-    // printf("] is [");
-    // printArray(7, result, resultcnt);
-    // printf("]\n\n");
+  // resultcnt = 0;
+  // array[0] = 2;
+  // array[1] = 3;
+  // array[2] = 5;
+  // printf("\nhowSum 8 of array [");
+  // printArray(8, array, 3);
+  // howSum(8, array, 3);
+  // printf("] is [");
+  // printArray(7, result, resultcnt);
+  // printf("]\n\n");
 
-    // resultcnt = 0;
-    // array[0] = 2;
-    // array[1] = 3;
-    // array[2] = 5;
-    // printf("\nhowSum 8 of array [");
-    // printArray(8, array, 3);
-    // howSum(8, array, 3);
-    // printf("] is [");
-    // printArray(7, result, resultcnt);
-    // printf("]\n\n");
+  // resultcnt = 0;
+  // array[0] = 7;
+  // array[1] = 14;
+  // printf("\nhowSum 300 of array [");
+  // printArray(300, array, 2);
+  // howSum(300, array, 2);
+  // printf("] is [");
+  // printArray(300, result, resultcnt);
+  // printf("]\n\n");
 
-    // resultcnt = 0;
-    // array[0] = 7;
-    // array[1] = 14;
-    // printf("\nhowSum 300 of array [");
-    // printArray(300, array, 2);
-    // howSum(300, array, 2);
-    // printf("] is [");
-    // printArray(300, result, resultcnt);
-    // printf("]\n\n");
-
-    return 0;
+  return 0;
 }
