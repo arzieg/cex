3b4373f7e366bc4ec79c167cc2df18399d3f9064
Arne <arne.ziegler@gmx.net>
2024-01-05 16:04:50 +0100

    Update demo.c

--- pf/test/demo.c
+++ pf/test/demo.c
@@ -6,45 +6,56 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIDLENGTH 4
+
 typedef struct {
   ssize_t length;
   char *string;
   size_t buffer_size;
-} FancyString;
+} CustomString;
 
-void FancyString_free(FancyString *target) {
+void CustomString_free(CustomString *target) {
   if (target->string) {
     free(target->string);
   }
   free(target);
 }
 
-FancyString *fancy_getline(FILE *stream) {
-  FancyString *new = malloc(sizeof(*new));
+CustomString *custom_getline(FILE *stream, int maxchars) {
+  do {
+    CustomString *new = malloc(sizeof(*new));
+    new->string = NULL;
+    new->buffer_size = 0;
+    new->length = getline(&(new->string), &(new->buffer_size), stream);
+    if ((new->length == -1) || (new->length != maxchars)) {
+      free(new);
+      printf("\nExpect %d characters, try again >> ", maxchars);
+    } else {
+      return new;
+    }
+  } while (true);
+}
+
+bool get_yesno_status(char *text, FILE *stream) {
+  bool answer = false;
+  char character;
+  printf(text);
+  CustomString *new = malloc(sizeof(*new));
   new->string = NULL;
   new->buffer_size = 0;
 
-  new->length = getline(&(new->string), &(new->buffer_size), stream);
-  if (new->length == -1) {
-    free(new);
-    return NULL;
-  } else {
-    return new;
-  }
-}
-
-bool get_yesno_status(char *text) {
-  char c;
-  bool answer;
   do {
-    printf("\nMCOS System? (y/n)");
-    scanf(" %c", &c);
-    c = toupper(c);
-
-    answer = (c == 'Y') || (c == 'N');
-
+    new->length = getline(&(new->string), &(new->buffer_size), stream);
+    strncpy(&character, new->string, 1);
+    character = toupper(character);
+    answer = (character == 'Y') || (character == 'N');
+    if (!answer) {
+      printf("Please enter y or n >> ");
+    }
   } while (!answer);
-  return (c == 'Y') ? true : false;
+
+  CustomString_free(new);
+  return (character == 'Y') ? true : false;
 }
 
 int main() {
@@ -53,21 +64,24 @@ int main() {
   char msg[255];
 
   for (int i = 0; i < 2; i++) {
-    FancyString *line = fancy_getline(stdin);
+    printf("\nPlease enter SID: ");
+    CustomString *line = custom_getline(stdin, SIDLENGTH);
     strncpy(text[i], line->string, line->length);
+    // add \0 Terminator at end of SID
+    text[i][line->length - 1] = '\0';
 
     printf("Read %zd bytes, buffer is %zd bytes\n", line->length,
            line->buffer_size);
-    printf("Line read:\n%s", line->string);
+    printf("Line read:%s\n", line->string);
 
-    strncpy(
-        msg,
-        "\n is this System part of a MCOS System with multiples SIDs (Y/N)?",
-        255);
-    mcos[i] = get_yesno_status(msg);
+    strncpy(msg,
+            "is the SAP SID part of a MCOS System with multiples SIDs (Y/N)?",
+            255);
+    mcos[i] = get_yesno_status(msg, stdin);
 
-    FancyString_free(line);
+    CustomString_free(line);
   }
+
   for (int i = 0; i < 2; i++) {
     printf("\nString %d = %s, mcos = %d", i, text[i], mcos[i]);
   }
