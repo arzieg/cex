29e2d63ba883928ee93c190bbc4b7c1b31d5130e
Arne <arne.ziegler@gmx.net>
2024-01-14 21:18:45 +0100

    add colon and alphanum check

--- pf/src/pfhelper.h
+++ pf/src/pfhelper.h
@@ -2,14 +2,18 @@
 #define PF_HELPER_
 
 #include <stdbool.h>
+#include <stdio.h>
 #include <sys/types.h>
 
 #include "pf.h"
 
-void CustomString_free(CustomString *target);
-bool CustomString_isalphanumeric(CustomString *target);
+enum stringfunction { ISALPHANUMERIC, ISALPHAORCOLON };
 
-CustomString *custom_getline(FILE *stream, int minchars, int maxchars);
+void CustomString_free(CustomString *target);
+bool CustomString_isalphaORcolon(CustomString *target);
+
+CustomString *custom_getline(FILE *stream, int minchars, int maxchars,
+                             int stringfunction);
 bool get_yesno_status(char *text, FILE *stream);
 void toUpperCase(char *line, int n);
 #endif
