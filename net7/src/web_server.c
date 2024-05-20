50a1b09c6eb864f998fc6806f6134e091f486484
Arne Ziegler <arne.ziegler@gmx.net>
2024-05-20 16:00:03 +0200

    update net7

--- net7/src/web_server.c
+++ net7/src/web_server.c
@@ -1,6 +1,21 @@
 /* web_server.c */
 #include "code07.h"
 
+/* Store Client information in a structure
+   as linked list */
+#define MAX_REQUEST_SIZE 2047
+struct client_info
+{
+    socklen_t address_length;
+    struct sockaddr_storage address;
+    SOCKET socket;
+    char request[MAX_REQUEST_SIZE + 1];
+    int received;
+    struct client_info *next;
+};
+// root-node
+static struct client_info *clients = 0;
+
 const char *get_content_type(const char *path)
 {
     const char *last_dot = strrchr(path, '.');
@@ -36,4 +51,74 @@ const char *get_content_type(const char *path)
             return "text/plain";
     }
     return "application/octet-stream";
+}
+
+SOCKET create_socket(const char *host, const char *port)
+{
+    printf("Configuring local address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo *bind_address;
+    getaddrinfo(host, port, &hints, &bind_address);
+    printf("Creating socket...\n");
+    SOCKET socket_listen;
+    socket_listen = socket(
+        bind_address->ai_family,
+        bind_address->ai_socktype,
+        bind_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_listen))
+    {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+    printf("Binding socket to local address...\n");
+    if (bind(socket_listen,
+             bind_address->ai_addr,
+             bind_address->ai_addrlen))
+    {
+        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+    freeaddrinfo(bind_address);
+
+    printf("Listening...\n");
+    if (listen(socket_listen, 10) < 0)
+    {
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+    return socket_listen;
+}
+
+struct client_info *get_client(SOCKET s)
+{
+    struct client_info *ci = clients;
+
+    while (ci)
+    {
+        if (ci->socket == s)
+            break;
+        ci = ci->next;
+    }
+    // client found, return info
+    if (ci)
+        return ci;
+
+    // client not found, create new client
+    struct client_info *n = (struct client_info *)calloc(1, sizeof(struct client_info));
+
+    if (!n)
+    {
+        fprintf(stderr, "Out of memory.\n");
+        exit(1);
+    }
+
+    n->address_length = sizeof(n->address);
+    n->next = clients;
+    clients = n;
+    return n;
 }
