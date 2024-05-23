2fae5872b8c707176f40b13beb5087a349d9bb0d
Arne Ziegler <arne.ziegler@gmx.net>
2024-05-23 20:16:21 +0200

    Update net8

--- net8/src/smtp_send.c
+++ net8/src/smtp_send.c
@@ -4,3 +4,27 @@
 
 #define MAXINPUT 512
 #define MAXRESPONSE 1024
+
+void get_input(const char *prompt, char *buffer)
+{
+    printf("%s", prompt);
+
+    buffer[0] = 0;
+    fgets(buffer, MAXINPUT, stdin);
+    const int read = strlen(buffer);
+    if (read > 0)
+        buffer[read - 1] = 0;
+}
+
+void send_format(SOCKET server, const char *text, ...)
+{
+    char buffer[1024];
+    va_list args;
+    va_start(args, text);
+    vsprintf(buffer, text, args);
+    va_end(args);
+
+    send(server, buffer, strlen(buffer), 0);
+
+    printf("Client: %s", buffer);
+}
