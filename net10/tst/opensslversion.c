0e33d5a706b060445b5fc3158a23cdc82930cb9b
Arne Ziegler <arne.ziegler@gmx.net>
2024-06-02 21:37:53 +0200

    Update net10

--- net10/tst/opensslversion.c
+++
@@ -1,8 +0,0 @@
-#include <openssl/ssl.h>
-#include <openssl/crypto.h>
-
-int main(int argc, char *argv[]) {
-		printf("OpenSSL version: %s\n", OpenSSL_version(SSLEAY_VERSION));
-		return 0;
-		}
-		
