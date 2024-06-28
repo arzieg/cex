2e4bd583de0413dd647100e3a9242eaa5648f69b
Arne Ziegler <arne.ziegler@gmx.net>
2024-06-28 22:08:31 +0200

    update ssh

--- ssh1/src/sshshell.h
+++ ssh1/src/sshshell.h
@@ -4,4 +4,4 @@ void printerrorcode(int errcode);
 int verify_knownhost(ssh_session session);
 int authenticate_pubkey(ssh_session session);
 int establish_shell_session(ssh_session session, ssh_channel *channel);
-int interactive_shell_session(ssh_session session, ssh_channel channel);
+int shell_cmd(ssh_channel channel, const char *cmd);
