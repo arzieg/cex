15df2843553c924fc4d1277d7dd2b2036d4c8e43
Arne Ziegler <arne.ziegler@gmx.net>
2024-08-06 21:26:02 +0200

    Calculate test

--- tr1/sample.c
+++ tr1/sample.c
@@ -6,13 +6,17 @@ int calculate(int number) {
   int sum = 0;
   int i = number - 1;
   float j = 0;
-  for (i; i > 0; i--) {
-    printf("i=%d   sum=%d\n", i, sum);
-    j = i / 3.0f;
-    if (j == abs(j)) sum += i;
+  float k = 0;
+  if (number > 0) {
+    for (i; i > 0; i--) {
+      printf("i=%d   sum=%d\n", i, sum);
+      j = i / 3.0f;
+      k = i / 5.0f;
 
-    j = i / 5.0f;
-    if (j == abs(j)) sum += i;
+      if (j == abs(j)) sum += i;
+      if (k == abs(k)) sum += i;
+      if (j == abs(j) && k == abs(k)) sum -= i;
+    }
   }
   return sum;
 }
@@ -25,6 +29,12 @@ int main() {
 
   result = calculate(10);
   assert(result == 23);
+
+  result = calculate(-5);
+  assert(result == 0);
+
+  result = calculate(20);
+  assert(result == 78);
 
   return 0;
 }
