c93eaa9e52bed8f2636466c5b205b0eef865a2b1
Arne Ziegler <arne.ziegler@edeka.de>
2024-10-07 17:16:13 +0200

    Add arg-example

---
+++ arg1/src/prog.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <argp.h> 
+
+static int 
+parse_opt (int key, char *arg, struct argp_state *state)
+{ 
+	 switch (key) 
+		  { 
+			   case 'd': printf (".\n"); break; 
+				      } 
+	  return 0; 
+} 
+int 
+main (int argc, char **argv) 
+{ 
+	 struct argp_option options[] = 
+		  { 
+			   { 0, 'd', 0, 0, "Show a dot on the screen",0}, 
+			    { 0 } 
+        	    }; 
+         struct argp argp = { options, parse_opt, 0, 0 ,0, 0, 0 }; 
+         return argp_parse (&argp, argc, argv, 0, 0, 0); 
+}
