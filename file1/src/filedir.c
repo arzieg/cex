5bbda901828b0dd1a57312971ad88ad04b39c133
Arne Ziegler <arne.ziegler@gmx.net>
2024-10-15 11:03:36 +0200

    Update filedir

--- file1/src/filedir.c
+++ file1/src/filedir.c
@@ -1,6 +1,10 @@
 #define _DEFAULT_SOURCE
 
+#include "filedir.h"
+#include "utils.h"
 #include <dirent.h>
+#include <regex.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,12 +12,10 @@
 #define NORMAL "\x1B[0m"
 #define GREEN "\x1B[32m"
 #define BLUE "\x1B[34m"
-
-// prototypes
-void get_dir (char *path);
+#define RED "\x1B[31m"
 
 void
-get_dir (char *path)
+get_dir (char *path, bool recursive, char *exclude)
 {
   DIR *d = opendir (path);
   if (d == NULL)
@@ -22,14 +24,62 @@ get_dir (char *path)
   while ((dir = readdir (d)) != NULL)
     {
       if (dir->d_type != DT_DIR)
-        printf ("  %s%s/%s\n", BLUE, path, dir->d_name);
+        {
+          bool skip = false;
+          char d_filename[257];
+          snprintf (d_filename, sizeof (d_filename), "%s/%s", path,
+                    dir->d_name);
+          FILE *file = fopen (exclude, "r");
+          if (file == NULL)
+            {
+              perror ("Error opening file");
+              exit (EXIT_FAILURE);
+            }
+
+          // Buffer to store each line
+          char pattern[256];
+
+          // Read each line from the file
+          while (fgets (pattern, sizeof (pattern), file))
+            {
+              pattern[strcspn (pattern, "\n")] = 0;
+              DEBUG_PRINT ("%sSearch for Pattern %s in file %s\n", NORMAL,
+                           pattern, d_filename);
+              regex_t regex;
+              int reti;
+
+              // compile regex
+              reti = regcomp (&regex, pattern, REG_EXTENDED);
+              if (reti)
+                {
+                  fprintf (stderr, "Could not compile regex\n");
+                  exit (EXIT_FAILURE);
+                }
+              reti = regexec (&regex, d_filename, 0, NULL, 0);
+              if (!reti)
+                {
+                  skip = true;
+                  continue;
+                }
+              // Free memory allocated to the pattern buffer by regcomp()
+              regfree (&regex);
+            }
+
+          // Close the file
+          fclose (file);
+          if (skip)
+            printf ("  %s<FILE>%s\n", GREEN, d_filename);
+          else
+            printf ("  %s<FILE>%s\n", BLUE, d_filename);
+        }
       else if (dir->d_type == DT_DIR && strcmp (dir->d_name, ".") != 0
                && strcmp (dir->d_name, "..") != 0)
         {
-          printf ("%s<DIR> %s\n", GREEN, dir->d_name);
+          // printf ("%s<DIR> %s\n", GREEN, dir->d_name);
           char d_path[257];
-          sprintf (d_path, "%s/%s", path, dir->d_name);
-          get_dir (d_path);
+          snprintf (d_path, sizeof (d_path), "%s/%s", path, dir->d_name);
+          if (recursive)
+            get_dir (d_path, recursive, exclude);
         }
     }
   closedir (d);
--- file1/src/utils.h
+++ file1/src/utils.h
@@ -5,6 +5,17 @@
 #define debug(...)
 #endif
 
+#define DEBUG 0
+
+#define DEBUG_PRINT(fmt, ...)                                                 \
+  do                                                                          \
+    {                                                                         \
+      if (DEBUG)                                                              \
+        fprintf (stderr, "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__,    \
+                 __VA_ARGS__);                                                \
+    }                                                                         \
+  while (0)
+
 #define FALSE 0
 
 void filedir_log (int severity, const char *format, ...)
